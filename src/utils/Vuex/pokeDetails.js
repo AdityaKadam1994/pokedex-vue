a82c485a43ec21e97a53c305f566fd737bae2362
Aditya <addy.7kad@gmail.com>
2020-07-04 15:43:50 +0530

    created separate axios instances added axios interceptors to limit api calls

--- src/utils/Vuex/pokeDetails.js
+++ src/utils/Vuex/pokeDetails.js
@@ -16,19 +16,34 @@ export const pokeDetails = {
   actions: {
     getData ({ commit }, payload) {
       const DATA = []
-      if (!storageData.pokedetail && !storageData.pokename) {
-        console.log(storageData)
-        for (let pokemonId = 0; pokemonId <= 32; pokemonId++) {
-          axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-            .then((res) => {
-              // console.log(res.data)
-              DATA.push(res.data)
-            })
-            .catch((err) => {
-              console.log(err)
-            })
+      const loadDataInstance = axios.create()
+      const loadNameInstance = axios.create()
+      loadDataInstance.interceptors.request.use(config => {
+        if (!storageData.pokedetail) {
+          return config
+        } else {
+          commit('POKEMONDATA', storageData)
         }
-        axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=964')
+      })
+      loadNameInstance.interceptors.request.use(config => {
+        if (!storageData.pokename) {
+          return config
+        } else {
+          commit('POKEMONDATA', storageData)
+        }
+      })
+      const multiData = async () => {
+        for (let pokemonId = 1; pokemonId <= 18; pokemonId++) {
+          await new Promise(resolve => {
+            loadDataInstance.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+              .then((res) => {
+                resolve(DATA.push(res.data))
+              })
+          })
+        }
+      }
+      multiData().then(() => {
+        loadNameInstance.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=964')
           .then(res => {
             const pokename = res.data.results.map((pk) => {
               return { name: pk.name }
@@ -36,10 +51,7 @@ export const pokeDetails = {
             commit('POKEMONDATA', { pokename: pokename, pokedetail: DATA })
           })
           .catch(err => console.log(err))
-      } else {
-        // const itemData = storageData
-        commit('POKEMONDATA', storageData)
-      }
+      })
     },
     singlePokemon ({ commit }, payload) {
       axios.get(`https://pokeapi.co/api/v2/pokemon/${payload}`)
